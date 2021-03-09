66eff9dc1ee91f59b88a0ea6fc87db12e9173ea1
diogogomes3 <diogogomes371@gmail.com>
2021-03-09 13:39:38 -0300

    alteração no nome do componente e ajuste nas propriedades do decorator

--- src/app/formulario-cliente/formulario-cliente.component.spec.ts
+++ src/app/formulario-cliente/formulario-cliente.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FormulariClienteComponent } from './formulari-cliente.component';
+import { FormulariClienteComponent } from './formulario-cliente.component';
 
 describe('FormulariClienteComponent', () => {
   let component: FormulariClienteComponent;
